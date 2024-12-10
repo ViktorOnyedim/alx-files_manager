d32cc5dee4946f19ad6c92f55b58c66357806501
ViktorOnyedim <viktoronyedim@gmail.com>
2024-12-10 17:18:54 +0100

    configured express server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = require('routes/index');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(routes);
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
+
+module.exports = app;
