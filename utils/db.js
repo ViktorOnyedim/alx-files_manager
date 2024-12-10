54c63beb63c4129bb281d6c72ad9d56646978227
ViktorOnyedim <viktoronyedim@gmail.com>
2024-12-10 16:56:24 +0100

    created mongodb client

---
+++ utils/db.js
@@ -0,0 +1,48 @@
+// import { MongoClient } from 'mongodb'
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+    constructor() {
+        const host = process.env.DB_HOST || 'localhost';
+        const port = process.env.DB_PORT || 27017;
+        const database = process.env.DB_DATABASE || 'files_manager';
+        const url = `mongodb://${host}:${port}`;
+
+        this.client = new MongoClient(url, { useUnifiedTopology: true });
+        this.dbName = database;
+
+        this.client.connect()
+            .then(() => console.log('Connected to MongoDB'))
+            .catch((err) => console.error('Failed to connect to MongoDB:', err));
+    }
+
+
+    isAlive() {
+        return this.client.isConnected();
+    }
+
+    async nbUsers() {
+        try {
+            const db = this.client.db(this.dbName);
+            const usersCollection = db.collection('users');
+            return await usersCollection.countDocuments();
+        } catch (err) {
+            console.error('Error getting number of users:', err);
+            return 0;
+        }
+    }
+
+    async nbFiles() {
+        try {
+            const db = this.client.db(this.dbName);
+            const filesCollection = db.collection('files');
+            return await filesCollection.countDocuments();
+        } catch (err) {
+            console.error('Error getting number of files:', err);
+            return 0;
+        }
+    }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
